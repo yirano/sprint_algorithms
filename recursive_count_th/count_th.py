88c75c486ae1259511965b2cf12c7389b575efac
yirano <yirano@protonmail.ch>
2020-10-11 21:35:44 -0400

    count_th take 2

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,7 +5,7 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 
 
-def count_th(word):
+def count_th_1(word):
     if word.find('th') > -1:
         rep = word.replace('th', '*')
         return count_th(rep)
@@ -13,6 +13,19 @@ def count_th(word):
     return word.count('*')
 
 
-# print(count_th("thhtthht"))
+def count_th(word):
+    find = 'th'
+    # if word < find stop the recursion and carry value up
+    if len(word) < len(find):
+        return 0
+    # if 'th' found, return 1
+    if word[0:len(find)] == find:
+        return count_th(word[len(find) - 1:]) + 1
+    # if not just keep going
+    return count_th(word[len(find) - 1:])
+
+
+print(count_th("thhtthht"))
 # print(count_th("abcthefthghith"))
 # print(count_th("abc"))
+# print(count_th("ath-bth-cth"))
